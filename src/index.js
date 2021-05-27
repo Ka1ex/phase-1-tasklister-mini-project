c85b8827f62c39eef05920027f6534562bb2ba23
ka1ex <kalexadamson@hotmail.com>
2021-05-27 13:22:22 -0500

    completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector("#create-task-form")
+  form.addEventListener('submit', (e) => {
+    const newTask = document.querySelector('#new-task-description').value
+    const  taskList = document.querySelector("#tasks")
+    const listElement = document.createElement("li")
+    e.preventDefault()
+   listElement.textContent += `${newTask}`
+   taskList.appendChild(listElement)
+   
+  })
+  
 });
+ 
